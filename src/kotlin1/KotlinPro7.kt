f59c7b42db37c10bf8903fade4aced59939d2e5d
ivansmoot <ivansmoot@163.com>
2021-02-18 21:25:37 +0800

    ?.和let语法

---
+++ src/kotlin1/KotlinPro7.kt
@@ -0,0 +1,25 @@
+package kotlin1
+
+class Person2 {
+
+    fun eat(){
+        println("eating...")
+    }
+
+    fun sleep(){
+        println("sleeping...")
+    }
+}
+
+fun live(person: Person2?){
+    person?.let {
+        it.eat()
+        it.sleep()
+    }
+}
+
+fun main() {
+    val person = Person2()
+    live(null)
+    live(person)
+}
