c1d5455ce3f49d8668433799e797554e57d8496b
ivansmoot <ivansmoot@163.com>
2021-02-19 14:30:29 +0800

    类委托

---
+++ src/kotlin1/KotlinPro10.kt
@@ -0,0 +1,23 @@
+package kotlin1
+
+// 委托
+// 类委托
+
+interface MyInterface2{
+    fun myPrint()
+}
+
+class MyInterfaceImpl(private val str: String): MyInterface2{
+    override fun myPrint() {
+        println("welcome $str")
+    }
+}
+
+// 委托构造方法的参数去实现myPrint方法
+class MyClass3(myinterface: MyInterface2): MyInterface2 by myinterface
+
+fun main() {
+    val myinterfaceimpl = MyInterfaceImpl("hello")
+    val myclass = MyClass3(myinterfaceimpl)
+    myclass.myPrint()
+}
