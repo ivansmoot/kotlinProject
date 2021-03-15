09089c386b00e8f452a71b33a290e87ab1ac2a37
ivansmoot <ivansmoot@163.com>
2021-03-15 18:33:56 +0800

    反射

--- src/reflection/KotlinPro8.kt
+++ src/reflection/KotlinPro8.kt
@@ -1,13 +1,14 @@
 package reflection
 
+import kotlin.reflect.full.functions
 import kotlin.reflect.full.memberFunctions
 import kotlin.reflect.full.superclasses
 
 interface Interface1{}
 
-class KotlinPro8(val name: String, val age: Int): Interface1{
+class KotlinPro8(val name: String, private val age: Int): Interface1{
     fun func1(){}
-    fun func2(): Int{return 1}
+    fun func2(num: Int): Int{return num + this.age}
 }
 
 fun main() {
@@ -15,4 +16,10 @@ fun main() {
     println(kt8.members)
     println(kt8.superclasses)
     println(kt8.memberFunctions)
+    println("----------")
+
+    val kot8 = KotlinPro8("ivan", 18)
+    val funcToInvoke = kt8.functions.find { it.name == "func2" }
+    val age2 = funcToInvoke?.call(kot8, 2)
+    println(age2)
 }
