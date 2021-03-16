344e62b2918f49831e8cf7c5a7461f4bfee6c36a
ivansmoot <ivansmoot@163.com>
2021-03-16 10:01:34 +0800

    反射,修改类属性

--- src/reflection/KotlinPro8.kt
+++ src/reflection/KotlinPro8.kt
@@ -1,14 +1,16 @@
 package reflection
 
+import kotlin.reflect.KMutableProperty
 import kotlin.reflect.full.functions
 import kotlin.reflect.full.memberFunctions
+import kotlin.reflect.full.memberProperties
 import kotlin.reflect.full.superclasses
 
 interface Interface1{}
 
 class KotlinPro8(val name: String, private val age: Int): Interface1{
     fun func(num: Int): Int{return num + this.age}
-    val add = "sh"
+    var add = "sh"
 }
 
 fun main() {
@@ -28,5 +30,13 @@ fun main() {
 
     // 获取类属性
     val address = kt8.members.find { it.name == "add" }
+    val address2 = kt8.memberProperties.find { it.name == "add" }
     println(address?.call(kot8))
+    println(address2?.call(kot8))
+
+    // 修改类属性
+    if (address2 is KMutableProperty<*>) {
+        address2.setter.call(kot8, "sh2")
+        println(address2?.call(kot8))
+    }
 }
